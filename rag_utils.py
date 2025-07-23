ed3f80a4751da7aaa9a03c37334954dbf34eb1f4
Fayez-Ali <fayezali2002@.com>
2025-07-23 22:39:37 +0500

    updated

--- rag_utils.py
+++ rag_utils.py
@@ -1,39 +1,3 @@
-# from langchain_community.document_loaders import PyPDFLoader
-# from langchain_text_splitters import RecursiveCharacterTextSplitter
-# from langchain_huggingface import HuggingFaceEmbeddings
-# from langchain_community.vectorstores import FAISS
-# from config import PDF_PATH, FAISS_INDEX
-# import os
-
-# def process_document():
-#     """Process the PDF into FAISS index"""
-#     loader = PyPDFLoader(PDF_PATH)
-#     documents = loader.load()
-    
-#     text_splitter = RecursiveCharacterTextSplitter(
-#         chunk_size=1000,
-#         chunk_overlap=200
-#     )
-#     chunks = text_splitter.split_documents(documents)
-    
-#     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-#     vector_db = FAISS.from_documents(chunks, embeddings)
-#     vector_db.save_local(FAISS_INDEX)
-#     return vector_db
-
-# def load_vector_db():
-#     """Load existing FAISS index"""
-#     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-#     if os.path.exists(FAISS_INDEX):
-#         return FAISS.load_local(FAISS_INDEX, embeddings, allow_dangerous_deserialization=True)
-#     return None
-
-# def get_relevant_docs(query, vector_db, k=3):
-#     """Retrieve relevant documents"""
-#     return vector_db.similarity_search(query, k=k)
-
-
-
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
